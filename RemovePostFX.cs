06a55e8ba77458893295bb94301fdc1c539be5ae
Ascyst <85650794+Ascyst@users.noreply.github.com>
2021-07-12 02:12:16 -0700

    Update to v2.0.0

--- RemovePostFX.cs
+++ RemovePostFX.cs
@@ -1,53 +1,89 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
-using UnboundLib;
-using BepInEx;
+﻿using BepInEx;
 using HarmonyLib;
+using System;
+using UnboundLib;
+using UnityEngine;
+using BepInEx.Configuration;
 
 namespace RemovePostFX
 {
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInPlugin(ModID, ModName, "2.0.0")]
+    [BepInProcess("Rounds.exe")]
+    public class RemovePostFX : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            rShakes = Config.Bind("RemovePostFX", "Remove Screen Shakes", true, "When enabled, removes screenshake.");
+            rRain = Config.Bind("RemovePostFX", "Remove Chromatic Aberration", true, "When enabled, removes chromatic aberration.");
+            rLight = Config.Bind("RemovePostFX", "Remove Light", true, "When enabled, removes the light and shadows from the game.");
+            rTerrain = Config.Bind("RemovePostFX", "Remove Ground Effects", true, "When enabled, removes the ground effects, making the ground gray.");
+            rPGlow = Config.Bind("RemovePostFX", "Remove Player Glow", true, "When enabled, removes the glow effect around the character. Also removes the pesky gun visual (overrated)");
+            On.Screenshaker.OnGameFeel += this.Screenshaker_OnGameFeel;
+            On.ChomaticAberrationFeeler.OnGameFeel += this.ChomaticAberrationFeeler_OnGameFeel;
+        }
 
-	[BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
-	[BepInPlugin(ModID, ModName, "1.0.0.0")]
-	[BepInProcess("Rounds.exe")]
-	public class RemovePostFX : BaseUnityPlugin
-	{
-		private void Awake()
-		{
-			On.Screenshaker.OnGameFeel += Screenshaker_OnGameFeel;
-			On.ChomaticAberrationFeeler.OnGameFeel += ChomaticAberrationFeeler_OnGameFeel;
-		}
+        private void Start()
+        {
+            new Harmony(ModID).PatchAll();
+            Unbound.RegisterGUI(ModName, new Action(this.DrawGUI));
+        }
 
-		private void Start()
-		{
-			Unbound.RegisterGUI(ModName, new Action(this.DrawGUI));
-		}
+        private void DrawGUI()
+        {
+            bool newrShakes = !GUILayout.Toggle(rShakes.Value, "Remove Screen Shakes");
+            bool newrRain = !GUILayout.Toggle(rRain.Value, "Remove Chromatic Aberration (Rainbow Explosions)");
+            bool newrTerrain = !GUILayout.Toggle(rTerrain.Value, "Remove colored ground (decent FPS boost)");
+            bool newrPGlow = !GUILayout.Toggle(rPGlow.Value, "Remove the glow effect around players. Also removes the pesky gun visual (overrated)");
+            bool newrLight = !GUILayout.Toggle(rLight.Value, "Remove the light from the top of the map, and the shadows it causes");
 
-		private void DrawGUI()
-		{
-			bool flagS = GUILayout.Toggle(RemovePostFX.rShakes, "Remove Shakes");
-			RemovePostFX.rShakes = flagS;
-			bool flagR = GUILayout.Toggle(RemovePostFX.rRain, "Remove Chromatic Aberration (Rainbows)");
-			RemovePostFX.rRain = flagR;
-		}
-		private void Screenshaker_OnGameFeel(On.Screenshaker.orig_OnGameFeel orig, Screenshaker self, Vector2 feelDirection)
-		{
-            orig(self, feelDirection * (rShakes ? 0 : 1));
-		}
-		private void ChomaticAberrationFeeler_OnGameFeel(On.ChomaticAberrationFeeler.orig_OnGameFeel orig, ChomaticAberrationFeeler self, Vector2 feelDirection)
-		{
-			orig(self, feelDirection * (rRain ? 0 : 1));
-		}
+            rPGlow.Value = newrPGlow;
+            rTerrain.Value = newrTerrain;
+            rLight.Value = newrLight;
+            rRain.Value = newrRain;
+            rShakes.Value = newrShakes;
+        }
+
+        private void Screenshaker_OnGameFeel(On.Screenshaker.orig_OnGameFeel orig, Screenshaker self, Vector2 feelDirection)
+        {
+            orig(self, feelDirection * (rShakes.Value ? 0 : 1));
+        }
+
+        private void ChomaticAberrationFeeler_OnGameFeel(On.ChomaticAberrationFeeler.orig_OnGameFeel orig, ChomaticAberrationFeeler self, Vector2 feelDirection)
+        {
+            orig(self, feelDirection * (rRain.Value ? 0 : 1));
+        }
 
 
-		private const string ModID = "com.ascyst.rounds.removepostfx";
+        private const string ModID = "com.ascyst.rounds.removepostfx";
 
-		private const string ModName = "Remove PostFX";
+        private const string ModName = "Remove PostFX";
 
-		public static bool rShakes;
+        public static ConfigEntry<bool> rShakes;
 
-		public static bool rRain;
+        public static ConfigEntry<bool> rRain;
 
-	}
+        public static ConfigEntry<bool> rTerrain;
+
+        public static ConfigEntry<bool> rPGlow;
+
+        public static ConfigEntry<bool> rLight;
+    }
+    [HarmonyPatch(typeof(ArtHandler), "NextArt")]
+    internal class ArtHandler_Patch
+    {
+        private static void Postfix()
+        {
+            foreach (var system in UnityEngine.Object.FindObjectsOfType<ParticleSystem>())
+            {
+                if (system.gameObject.name.Contains("Skin_Player"))
+                {
+                    system.SetPropertyValue("enableEmission", RemovePostFX.rPGlow.Value);
+                }
+            }
+            GameObject.Find("BackgroudParticles").SetActive(RemovePostFX.rTerrain.Value);
+            GameObject.Find("FrontParticles").SetActive(RemovePostFX.rTerrain.Value);
+            GameObject.Find("LightShake").SetActive(RemovePostFX.rLight.Value);
+        }
+    }
 }
